a1f4724e977b1441c3763f07640a6dfc31ab2b0e
chensha <103901302@qq.com>
2017-11-02 11:43:00 +0800

    erp init version

---
+++ application/admin/controller/Articlelist.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace app\admin\controller;
+use app\common\controller\Backend;
+
+
+
+class Articlelist  extends Backend
+{
+    public function index()
+    {
+       return $this->view->fetch();
+    }
+
+
+
+}
+
---
+++ application/common/controller/Backend.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace app\common\controller;
+use app\common\model\Configvalue;
+use think\Config;
+use think\Controller;
+use think\Lang;
+use think\Session;
+/**
+ * 后台控制器基类
+ */
+class Backend extends Controller
+{
+
+    /**
+     * 返回码,默认为null,当设置了该值后将输出json数据
+     * @var int
+     */
+    protected $code = null;
+
+    /**
+     * 返回内容,默认为null,当设置了该值后将输出json数据
+     * @var mixed
+     */
+    protected $data = null;
+
+    /**
+     * 返回文本,默认为空
+     * @var mixed
+     */
+    protected $msg = '';
+
+    /**
+     * 布局模板
+     * @var string
+     */
+    protected $layout = 'default';
+
+    public function _initialize()
+    {
+        $modulename = $this->request->module();
+        $controllername = strtolower($this->request->controller());
+        $actionname = strtolower($this->request->action());
+
+        $path = '/' . $modulename . '/' . str_replace('.', '/', $controllername) . '/' . $actionname;
+
+        // 定义是否Addtabs请求
+        !defined('IS_ADDTABS') && define('IS_ADDTABS', input("addtabs") ? TRUE : FALSE);
+
+        // 定义是否Dialog请求
+        !defined('IS_DIALOG') && define('IS_DIALOG', input("dialog") ? TRUE : FALSE);
+
+        // 定义是否AJAX请求
+        !defined('IS_AJAX') && define('IS_AJAX', $this->request->isAjax());
+
+        $lang = Lang::detect();
+
+        // 非选项卡时重定向
+        if (!$this->request->isPost() && !IS_AJAX && !IS_ADDTABS && !IS_DIALOG && input("ref") == 'addtabs')
+        {
+            $url = preg_replace_callback("/([\?|&]+)ref=addtabs(&?)/i", function($matches) {
+                return $matches[2] == '&' ? $matches[1] : '';
+            }, $this->request->url());
+            $this->redirect('index/index', [], 302, ['referer' => $url]);
+            exit;
+        }
+          // 如果有使用模板布局
+        if ($this->layout)
+        {
+            $this->view->engine->layout('layout/' . $this->layout);
+        }
+           // 配置信息
+        $config = [
+            'modulename'     => $modulename,
+            'controllername' => $controllername,
+            'actionname'     => $actionname,
+            'jsname'         => 'backend/' . str_replace('.', '/', $controllername),
+            'moduleurl'      => url("/{$modulename}", '', false),
+            'language'       => $lang,
+            'referer'        => Session::get("referer")
+        ];
+
+        // 如果有使用模板布局
+
+        // 语言检测
+        $this->assign('config', $config);
+        $lang = Lang::detect();
+        $this->loadlang($controllername);
+    }
+    
+    /**
+     * 加载语言文件
+     * @param string $name
+     */
+    protected function loadlang($name)
+    {
+        Lang::load(APP_PATH . $this->request->module() . '/lang/' . Lang::detect() . '/' . str_replace('.', '/', $name) . '.php');
+    }
+
+
+    /**
+     * 析构方法
+     *
+     */
+    public function __destruct()
+    {
+        //判断是否设置code值,如果有则变动response对象的正文
+        if (!is_null($this->code))
+        {
+           //var_dump(IS_DIALOG);
+           $this->result($this->data, $this->code, $this->msg, 'json');
+        }
+    }
+
+}
