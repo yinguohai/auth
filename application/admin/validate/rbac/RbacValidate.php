7fe9a5416f3caa363ffa1d130a869c22b23fcf18
yinguohai <yi.gh@jiahongnet.com>
2017-11-16 18:54:45 +0800

    权限保存代码抽离

--- application/admin/validate/rbac/RbacValidate.php
+++ application/admin/validate/rbac/RbacValidate.php
@@ -7,20 +7,23 @@ class RbacValidate extends Validate
     protected $rule=[
         'r_name'=>'require|max:16',
         'o_name'=>'require',
-        'g_name'=>'require'
+        'g_name'=>'require',
+        'a_title'=>'require',
     ];
     //提示信息
     protected $msg=[
         'r_name.require'=>'角色名称必填',
         'r_name.max'=>'角色名称长度最多16个字符',
         'o_name.require'=>'组织名称不能为空',
-        'g_name.require'=>'组名称不能为空'
+        'g_name.require'=>'组名称不能为空',
+        'a_title.require'=>'权限title不能为空'
     ];
     //验证场景
     protected $scene=[
         'Role'=>['r_name'],
         'Organize'=>['o_name'],
-        'Group'=>['g_name']
-
+        'Group'=>['g_name'],
+        'Access'=>['a_tilte'],
+        ''
     ];
 }
